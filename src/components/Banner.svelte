e9b864c0602ff9066bfa00aa297c05d1d6ba66a0
yllekjjirpvul98 <kyungchingman@gmail.com>
2021-02-10 01:56:04 +0800

    Finish index page

---
+++ src/components/Banner.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<style>
+
+@keyframes -global-blinker {
+  50% {
+    opacity: 0;
+  }
+}
+
+#banner p{
+  position: relative;
+  font-family: 'Press Start 2P', cursive;
+  top: auto;
+  animation: blinker 1s step-start infinite;
+}
+
+#banner {
+  border: 3px white dotted;
+  border-radius: 15px;
+  background-color: rgb(2, 81, 150);
+  text-align: center;
+  font-size: 20;
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  margin: 0 auto;
+  max-width: 80%;
+}
+
+#banner p {
+    padding-left: 2em;
+    padding-right: 2em;    
+}
+
+</style>
+
+<div id="banner">
+      <p> ▼ Navigate to find out more about ME! 👾▼ </p>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,11 +7,12 @@
 <style>
 	main {
 		position: relative;
-		max-width: 56em;
-		background-color: white;
+		max-width: 80%;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
+		height: 100%;
+		width: 100%;
 	}
 </style>
 
